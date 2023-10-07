fa6027584c65a0bfd3ce932443b05f03a093e451
Beata <beata.lochowska@gmail.com>
2023-10-07 16:39:38 +0200

    adding styling to links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { PageHeader } from "@/ui/organisms/PageHeader";
 import { ProductList } from "@/ui/organisms/ProductList";
 import { type ProductItemType } from "@/ui/types";
 
@@ -48,7 +47,6 @@ const products: ProductItemType[] = [
 export default function Home() {
   return (
     <section className='mx-auto, max-w-md p-12 sm:max-w-2xl sm: py-16 md:max-w-4xl lg:max-w-7xl'>
-      <PageHeader />
       <ProductList products={products} />
     </section>
   )
--- src/ui/organisms/PageHeader.tsx
+++ src/ui/organisms/PageHeader.tsx
@@ -1,10 +1,14 @@
-import Link from "next/link"
+"use client"
+
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 export const PageHeader = () => {
+    const pathname = usePathname();
     return (
         <section role="navigation">
-            <Link href="/">Home</Link>
-            <Link href="/products">All</Link>
+            <Link aria-current='page' href="/" className={`${pathname === '/' ? 'font-bold' : 'font-medium'}`}>Home</Link>
+            <Link href="/products" className={`${pathname === '/products' ? 'font-bold' : 'font-medium'}`}>All</Link>
         </section>
     )
 }
